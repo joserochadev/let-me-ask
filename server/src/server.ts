dc39ebfcbbaf6a6bd9284754c4e531ad7bb87eeb
José Rocha <joserochadev@gmail.com>
2025-07-25 14:59:37 -0300

    feat: specify host in server listen configuration

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import { app } from "./app.js";
 
 const port = Number(process.env.PORT) || 3333;
 
-app.listen({ port }).then(() => {
+app.listen({ port, host: "0.0.0.0" }).then(() => {
   // biome-ignore lint/suspicious/noConsole: <explanation>
   console.log("HTTP server running...");
 });
